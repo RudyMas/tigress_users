4f56a126843707544abf0130063ef861925a4dd4
RudyMasGoNext <rudy.mas@go-next.be>
2025-12-09 15:13:00 +0100

    Update to PHP 8.5

--- src/Users.php
+++ src/Users.php
@@ -3,12 +3,12 @@
 namespace Tigress;
 
 /**
- * Class Users (PHP version 8.4)
+ * Class Users (PHP version 8.5)
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2025 Rudy Mas (https://rudymas.be)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.10.16.0
+ * @version 2025.12.09.0
  * @package Tigress\FormViewer
  */
 class Users
@@ -20,6 +20,6 @@ class Users
      */
     public static function version(): string
     {
-        return '2025.10.16';
+        return '2025.12.09';
     }
 }
